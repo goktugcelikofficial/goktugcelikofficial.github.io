8718235fbb02a2228ef5586dce0d4fb0916e7b41
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-11-20 01:29:31 +0300

    +

--- js/gün-tarih-saat.js
+++ js/gün-tarih-saat.js
@@ -40,7 +40,7 @@ princewedding.oncountup=function(result){
     //result is an object containing the current count up date/time, updated every second
     //Available properties: result["days"], result["hours"], result["minutes"], and result["seconds"]
     var mycountainer=document.getElementById("cpcontainer")
-    mycountainer.innerHTML="Çalışma süresi: <b>"+result['days']+"</b> gündür aktif."
+    mycountainer.innerHTML="Çalışma süresi: <a style='color:red'>"+result['days']+"</a> gündür aktif."
 }
 
 // TARİH AYARI //
@@ -58,7 +58,7 @@ var days=new Array("Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","
 var months=new Array("Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık");
 if (s<10) {s="0" + s}
 if (m<10) {m="0" + m}
-document.getElementById("clock").innerHTML= "<b>Tarih:</b> " + date + " " + months[month] + " " + year + " | <b>Günlerden:</b> " + days[day] + " | <b>Saat:</b> " + h + ":" + m + ":" + s + " "
+document.getElementById("clock").innerHTML= "Tarih: <a style='color:red'>" +date+ " "+months[month]+" " +year+ "</a> | Günlerden: <a style='color:red'>" +days[day]+ "</a> | Saat: <a style='color:red'>" + h + ":" + m + ":" + s + "</a> "
 setTimeout("refrClock()",1000);
 }
 refrClock();
